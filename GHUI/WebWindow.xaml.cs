41ca0dd5dda031456045cb441589dee30111ee5f
Petr Mitev <p.mitevpi@gmail.com>
2020-05-01 14:36:40 -0400

    external html read

--- GHUI/WebWindow.xaml.cs
+++ GHUI/WebWindow.xaml.cs
@@ -18,20 +18,35 @@ namespace GHUI
         private mshtml.HTMLDocument _doc => (mshtml.HTMLDocument) webBrowser1.Document;
         private mshtml.HTMLInputTextElement _input => _doc.getElementById("fname") as mshtml.HTMLInputTextElement;
 
-        //private string _inputValue => _input?.value;
-
-
         private string _htmlString =
             "<html><head></head><body>First row<br>Second row<br><input type='text' id='fname' name='fname'><br><br></body></html>";
+
+        private string _htmlString2 => ReadHtml();
+        private string _htmlStringContainer;
 
         public WebWindow()
         {
             InitializeComponent();
-            webBrowser1.NavigateToString(_htmlString);
+            webBrowser1.NavigateToString(_htmlString2);
+            ReadHtml();
         }
 
         private void Test()
         {
+        }
+
+        private string ReadHtml()
+        {
+            string path = Assembly.GetExecutingAssembly().Location;
+            string directory = Path.GetDirectoryName(path);
+
+            string file = Path.Combine(directory, "Window.html");
+            if (File.Exists(file))
+            {
+                _htmlStringContainer = file;
+            }
+
+            return File.ReadAllText(_htmlStringContainer);
         }
 
 
