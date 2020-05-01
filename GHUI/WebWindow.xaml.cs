6ddfe7c6a8f1e43a9cfcb5fc860d794528c3c19e
Petr Mitev <p.mitevpi@gmail.com>
2020-05-01 16:50:51 -0400

    better watcher, no loop

--- GHUI/WebWindow.xaml.cs
+++ GHUI/WebWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace GHUI
         // HTML READ
         private string HtmlString2 => ReadHtml();
         private string _htmlStringContainer = "";
+        private FileSystemWatcher watcher;
 
         public string Value
         {
@@ -72,12 +73,6 @@ namespace GHUI
             if (!File.Exists(file)) return _htmlStringContainer;
             _htmlStringContainer = file;
 
-            //FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            //using (StreamReader sr = new StreamReader(fs))
-            //{
-            //    sr.
-            //}
-
             return File.ReadAllText(_htmlStringContainer);
         }
 
@@ -91,40 +86,27 @@ namespace GHUI
 
         public void MonitorTailOfFile()
         {
-            Task.Run(() =>
+            watcher = new FileSystemWatcher
             {
-                using (FileSystemWatcher watcher = new FileSystemWatcher())
-                {
-                    watcher.Path = Directory;
-
-                    // Watch for changes in LastAccess and LastWrite times, and
-                    // the renaming of files or directories.
-                    watcher.NotifyFilter = NotifyFilters.LastAccess
-                                           | NotifyFilters.LastWrite
-                                           | NotifyFilters.FileName
-                                           | NotifyFilters.DirectoryName;
-
-                    // Only watch text files.
-                    watcher.Filter = "*.html";
-
-                    // Add event handlers.
-                    watcher.Changed += OnChanged;
-
-                    // Begin watching.
-                    watcher.EnableRaisingEvents = true;
-
-                    // Wait for the user to quit the program.
-                    //Console.WriteLine("Press 'q' to quit the sample.");
-                    while (Console.Read() != 'q') ;
-                }
-            });
+                Path = Directory,
+                NotifyFilter = NotifyFilters.LastAccess
+                               | NotifyFilters.LastWrite
+                               | NotifyFilters.FileName
+                               | NotifyFilters.DirectoryName,
+                Filter = "*.html"
+            };
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
         }
 
         private void OnChanged(object source, FileSystemEventArgs e)
         {
             // Specify what is done when a file is changed, created, or deleted.
+            watcher.Dispose();
+            watcher = null;
             webBrowser1.NavigateToString(HtmlString2);
             Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+            MonitorTailOfFile();
         }
     }
 }
