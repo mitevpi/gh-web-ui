75d5fa2346c905d24cf7ccc0ca4634683b47c32b
Petr Mitev <p.mitevpi@gmail.com>
2020-05-17 11:10:04 -0400

    added generic cefsharp app

---
+++ WpfChromium/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using CefSharp;
+using CefSharp.Wpf;
+
+namespace WpfChromium
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ChromiumWebBrowser ChromeBrowser;
+        public MainWindow()
+        {
+            InitializeCef();
+            InitializeComponent();
+        }
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            ChromeBrowser = new ChromiumWebBrowser("http://ourcodeworld.com");
+            // Add it to the form and fill it to the form window.
+            //this.Controls.Add(chromeBrowser);
+            //chromeBrowser.Dock = DockStyle.Fill;
+        }
+
+        private void InitializeCef()
+        {
+            Cef.EnableHighDPISupport();
+
+            string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            string assemblyPath = Path.GetDirectoryName(assemblyLocation);
+            string pathSubprocess = Path.Combine(assemblyPath, "CefSharp.BrowserSubprocess.exe");
+            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+            CefSettings settings = new CefSettings
+            {
+                LogSeverity = LogSeverity.Verbose,
+                LogFile = "ceflog.txt",
+                BrowserSubprocessPath = pathSubprocess,
+            };
+
+            settings.CefCommandLineArgs.Add("allow-file-access-from-files", "1");
+            settings.CefCommandLineArgs.Add("disable-web-security", "1");
+            Cef.Initialize(settings);
+        }
+    }
+}
