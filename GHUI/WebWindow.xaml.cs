3fb15b006b64be257fa78a97df8274007b4d4677
Deyan Nenov <dnenov@archilizer.com>
2021-03-21 13:45:48 +0000

    Made it work.
    1. Installed the latest prerelase version of WebView2 (not sure if it was crucial).
    2. Create webView2 control programatically and after the panel hosting the browser is created
    3. Async initialize
    4. Make sure all dlls are copied in the library folder!!! (especially the WebView2Loader.dll)

--- GHUI/WebWindow.xaml.cs
+++ GHUI/WebWindow.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Wpf;
 using mshtml;
 
 namespace GHUI
@@ -25,6 +30,8 @@ namespace GHUI
         /// List of the id properties of all the input elements in the DOM.
         /// </summary>
         public List<string> InputIds;
+        private WebView2 webView;
+        private string location = "";
 
 
         /// <summary>
@@ -34,11 +41,51 @@ namespace GHUI
         public WebWindow(string path)
         {
             _path = path;
+            location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\temp";
             InitializeComponent();
+
             //WebBrowser.Source = "https://www.microsoft.com";
             //WebBrowser.Source = new Uri(_path);
             //WebBrowser.NavigateToString(HtmlString);
             //WebBrowser.LoadCompleted += BrowserLoaded;
+        }
+
+
+        internal async void Navigate()
+        {
+            webView = new WebView2();  //Create a webview2 control programatically
+
+            this.Docker.Children.Clear();
+            this.Docker.Children.Add(webView);  //Add the webview2 to the dock panel
+
+            await InitializeAsync(webView);
+
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                //navigate to website 
+                webView.Source = new Uri(_path);
+            }
+        }
+
+        private async Task InitializeAsync(WebView2 webView)
+        {
+            // wait for coreWebView2 initialization
+            try
+            {
+                MessageBox.Show($"Location {@location}");
+                var env = await CoreWebView2Environment.CreateAsync(null, @location);
+                await webView.EnsureCoreWebView2Async(env);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Attempt to navigate to gh page..");
+            Navigate();
         }
     }
 }
